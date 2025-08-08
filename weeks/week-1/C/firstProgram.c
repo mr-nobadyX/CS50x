be89baa82134263e2310302d021ca7c6351287e4
mr-nobadyX <sanjaykushwaha0573@gmail.com>
2025-08-08 19:47:23 +0530

    Done with 1st week Lec

--- weeks/week-1/C/firstProgram.c
+++ weeks/week-1/C/firstProgram.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-int main() {
-    printf("Hello, CS50!\n");
-    return 0;
+int main(void) {
+    // char name[100]; // buffer to store the name
+
+    // printf("What's your name? ");
+    // scanf("%99s", name); // read up to 99 characters, leaving space for '\0'
+
+    // printf("hello, %s\n", name);
+    // return 0;
+
 }
